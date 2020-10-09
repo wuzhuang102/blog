1862ad71452c272bcc5feb9a95ec6293261ecfc8
jack_wu <1021222544@qq.com>
2020-10-09 10:53:11 +0800

    vue3 init

--- functional/index.js
+++
@@ -1,8 +0,0 @@
-var curry = require('loadsh').curry
-
-var match = curry(function(what, str) {
-    return str.match(what)
-})
-
-console.log(match(/o/g, 'Hello World'))
-console.log(match(/o/g)('Hello World'))
